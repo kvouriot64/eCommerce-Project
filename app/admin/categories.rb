1f4b84af03b42421d0f388ef4d9ac6e45806376e
Kyle <kvouriot64@yahoo.com>
2020-04-07 15:09:30 -0500

    Updated category and product active admin pages

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,18 +1,5 @@
-ActiveAdmin.register Category do
+# frozen_string_literal: true
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :category
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:category]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+ActiveAdmin.register Category do
+  permit_params :category
 end
--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,41 +1,30 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Product do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  permit_params :category, :product_name, :description, :price, :image, :OrderStatus_id, :Order_status_id, category_products: %i[id Category_id Product_id _destroy]
+  permit_params :category, :product_name, :description, :price, :image, category_products_attributes: %i[id category_id product_id _destroy]
 
-  # show do |category|
-  #   attributes_table
-  # end
+  show do |_product|
+    attributes_table do
+      row :product_name
+      row :description
+      row :price
+      row :categories do |product|
+        product.categories.map(&:category).join(', ').html_safe
+      end
+    end
+  end
 
   form do |f|
     f.semantic_errors
-    f.inputs
-    f.inputs do
+    f.inputs 'product' do
+      f.input :product_name
+      f.input :description
+      f.input :price
+      f.has_many :category_products, allow_destroy: true do |n_f|
+        n_f.input :category
+      end
       f.input :image, as: :file
     end
-
-    # f.inputs 'Product' do
-    #   f.has_many :category_products, allow_destroy: true do |n_f|
-    #     n_f.input :category
-    #   end
-
-    #   f.has_many :order_products, allow_destroy: true do |n_f|
-    #     n_f.input :order
-    #   end
-    # end
     f.actions
   end
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:product_name, :description, :price, :OrderStatus_id, :Order_status_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
 end
