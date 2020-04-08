5f93c82fc9fa0a0313fcb43278ae87e283a32286
Kyle <kvouriot64@yahoo.com>
2020-04-08 16:22:39 -0500

    replaced about and contact model with a single, pages model

--- app/models/about.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class About < ApplicationRecord
-  validates :title, :content, presence: true
-end
--- app/models/contact.rb
+++
@@ -1,3 +0,0 @@
-class Contact < ApplicationRecord
-  validates :title, :email, :phone_number, presence: true
-end
